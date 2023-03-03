302d7ae246634d46ab17047be8864cca10456214
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2023-03-02 22:15:42 -0500

    add mysql to the project

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const port = 3000;
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'testDB',
+	port: 3306,
+});
+db.connect((err) => {
+	if (err) throw err;
+	console.log('Connection to MySql database succeeded.');
+});
+
 
 app.get('/', (req, res) => {
 	res.send('CS348!!!!');
 });
+
+app.get('/courses', async (req, res) => {
+	db.query('SELECT * FROM courses',
+		(err, results) => {
+			if (err) throw err
+			res.send(results)
+		})
+});
+
 
 app.listen(port, () => {
 	console.log(`App listening on http://localhost:${port}`);
