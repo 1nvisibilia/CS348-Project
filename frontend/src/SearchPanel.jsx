2656f91034944758f11bb60fda3c52c4d94970fc
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2023-03-05 14:22:25 -0500

    frontend, axios

--- frontend/src/SearchPanel.jsx
+++ frontend/src/SearchPanel.jsx
@@ -3,19 +3,29 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
+import axios from 'axios';
 
+const courseSubs = [
+    'Any', 'CS', 'ECE', 'MATH', 'AMATH', 'PMATH', 'SE', 'STAT', 'CO', 'A', 'B', 'Bf', '3B', 'D1', '34B'
+];
 
 export default function SearchPanel() {
-    const courseAbrs = [
-        'Any', 'CS', 'ECE', 'MATH', 'AMATH', 'PMATH', 'SE', 'STAT', 'CO', 'A', 'B', 'Bf', '3B', 'D1', '34B'
-    ];
-    const [courseAbr, setCourseAbr] = useState('Any');
+    const [courseSub, setCourseSub] = useState('Any');
     const [courseCode, setCourseCode] = useState('N/A');
-    const [courseName, setCourseName] = useState('N/A');
 
     const changeCourseAbr = (event) => {
         setCourseAbr(event.target.value);
     }
+    const searchQuery = async () => {
+        const response = await axios.get('/search', {
+            params: {
+                'courseSub': 'CS',
+                'courseCode': '343'
+            }
+        });
+        console.log(response);
+    };
+    searchQuery();
 
     return (
         <div>
@@ -23,14 +33,14 @@ export default function SearchPanel() {
                 <InputLabel id="demo-simple-select-standard-label">Course Name</InputLabel>
                 <Select sx={{ maxWidth: 100 }}
                     id="demo-simple-select"
-                    value={courseAbr}
+                    value={courseSub}
                     label="Age"
-                    onChange={changeCourseAbr}
+                    onChange={setCourseSub}
                     displayEmpty
                     MenuProps={{ PaperProps: { sx: { maxHeight: 200 } } }}
                 >
                     {
-                        courseAbrs.map(e => <MenuItem key={e} value={e}>{e}</MenuItem>)
+                        courseSubs.map(e => <MenuItem key={e} value={e}>{e}</MenuItem>)
                     }
                 </Select>
             </FormControl>
