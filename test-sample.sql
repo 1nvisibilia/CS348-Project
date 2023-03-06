aa893411e85a90f5f6a6da13643a8917385b5adf
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2023-03-06 17:30:38 -0500

    update test sample files

--- test-sample.sql
+++ test-sample.sql
@@ -2,39 +2,17 @@
 
 # Display a user's friends they have added by their id, first and last names
 # Assume userID = 50000000
-SELECT DISTINCT friendeeid, c.id as cid, csub, cnum
-FROM friends, component as c
-WHERE frienderid = 50000000
-AND c.id IN (
-	SELECT cid
-    FROM attends
-    WHERE sid = frienderid
-) AND c.id IN (
-	SELECT cid
-    FROM attends
-    WHERE sid = friendeeid
-);
-
-
-# Display course codes that are attended by both the user and their friend
-# Assume userID = 50000000 and friendID = 40000000
-SELECT DISTINCT cid, csub, cnum
-FROM attends a INNER JOIN component c
-ON cid = c.id
-WHERE cid IN (
-	SELECT cid
-    FROM attends
-    WHERE sid = 40000000
-)
-AND
-cid IN (
-	SELECT cid
-    FROM attends
-    WHERE sid = 50000000
-);
+SELECT friendeeid, first_name, last_name, cid, csub, cnum
+FROM student INNER JOIN (
+	SELECT DISTINCT friendeeid, C.id AS cid, csub, cnum
+	FROM friends, component AS C
+	WHERE frienderid = 50000000
+		AND C.id IN (SELECT cid FROM attends WHERE sid = frienderid) 
+		AND C.id IN (SELECT cid FROM attends WHERE sid = friendeeid)) AS T
+ON id = friendeeid;
 
 # R7 Feature 2
-# Assume user has searched for 'CS 348'
+# Assume user has searched for 'CS 343'
 
 SELECT *
 FROM course
@@ -58,13 +36,25 @@ AND cnum = 348
 and assocnum = 1;
 
 # R9 Feature 4
-# These are only update, delete, and create trigger statements which have no output
+# These are only insert, delete, and create trigger statements which have no output
+# We leave the following as examples for insert and delete on attends for CS 343
+INSERT INTO attends
+    VALUES (10000000, 6125), (10000000, 6407);
 
+DELETE FROM attends
+    WHERE sid = 10000000 AND Cid IN
+    (SELECT id FROM component
+		WHERE csub = 'CS'
+		AND cnum = 343
+		AND assocnum = 1);
+		
 # R10 Feature 5
 
 SELECT *
 FROM component
 WHERE enrolltot < enrollcap
 AND csub = 'CS'
-AND cnum = 245;
+AND cnum = 350
+ORDER BY starttime, component.weekday DESC;
+
 
