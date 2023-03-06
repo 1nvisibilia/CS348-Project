514666ea7b81c7f7c237cfb9f013ce77f95ea2a1
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2023-03-05 20:06:19 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'password',
-	database: 'testDB',
+	database: 'myschedule',
 	port: 3306,
 });
 db.connect((err) => {
@@ -25,31 +25,28 @@ app.get('/', (req, res) => {
 	res.send('CS348!!!!');
 });
 
-app.get('/courses', async (req, res) => {
-	db.query('SELECT * FROM courses',
-		(err, results) => {
-			if (err) throw err
-			res.send(results)
-		})
-});
-
 app.get('/search', async (req, res) => {
-	console.log(req.query);
 	/** req.query:
 	 * {
 			'courseSub': string | null,
 			'courseCode': string | null
 		}
-	 */
 
-	// sample stub
-	res.json([
-		{ csub: 'CS', cnum: '102' },
-		{ csub: 'CS', cnum: '240' },
-		{ csub: 'CS', cnum: '241' },
-		{ csub: 'CS', cnum: '242' },
-		{ csub: 'CS', cnum: '350' }
-	]);
+		
+		// sample stub
+		res.json([
+			{ csub: 'CS', cnum: '102' },
+			{ csub: 'CS', cnum: '240' },
+			{ csub: 'CS', cnum: '241' },
+			{ csub: 'CS', cnum: '242' },
+			{ csub: 'CS', cnum: '350' }
+		]);
+	 */
+	db.query('SELECT csub, cnum FROM course',
+		(err, results) => {
+			if (err) throw err;
+			res.send(results)
+	})
 })
 
 
