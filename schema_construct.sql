505acb71756cac3b69ffb014b940c14c664e0adb
hennyg888 <henryg2002@hotmail.com>
2023-03-27 16:48:04 -0400

    added cap decr triggers

--- schema_construct.sql
+++ schema_construct.sql
@@ -211,3 +211,25 @@ BEGIN
 END$$
 DELIMITER ;
 
+DELIMITER $$
+CREATE TRIGGER cap_decr_delete
+AFTER DELETE ON Attends
+FOR EACH ROW
+BEGIN
+	UPDATE Component
+    SET enrolltot = enrolltot-1
+    WHERE id = OLD.cid;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER cap_decr_update
+AFTER UPDATE ON Attends
+FOR EACH ROW
+BEGIN
+	UPDATE Component
+    SET enrolltot = enrolltot-1
+    WHERE id = OLD.cid;
+END$$
+DELIMITER ;
+
