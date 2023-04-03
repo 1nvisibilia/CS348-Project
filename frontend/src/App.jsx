0ca30f894639fd674dd742fea3e5f928f35a0a98
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2023-04-02 22:50:15 -0400

    use material ui themes (#21)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,26 @@ import DashBoard from './DashBoard'
 import Banner from './Banner'
 import Login from './Login';
 import Admin from './Admin';
+import { useMediaQuery } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
 
+const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
+
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
 function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const [auth, login] = useState(null);
     const [admin, setAdmin] = useState(false);
     const loggedin = (username, admin) => {
@@ -24,12 +41,17 @@ function App() {
     }
 
     return (
-        <div className="App" style={{ width: "100vw", height: "100vh", padding: '0', margin: '0' }}>
-            <Banner></Banner>
-            {
-                mainPage()
-            }
-        </div>
+        <ThemeProvider theme={prefersDarkMode ? darkTheme : lightTheme}>
+            <CssBaseline />
+            <div className="App" style={{ width: "100vw", height: "100vh", padding: '0', margin: '0' }}>
+                <Banner></Banner>
+                {
+                    mainPage()
+                }
+            </div>
+        </ThemeProvider>
+
+
     )
 }
 
